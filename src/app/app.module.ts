fd3980d940d56518adddd609ee9e4ab8ae2730b5
Angel-Rey11 <91887811+Angel-Rey11@users.noreply.github.com>
2023-02-02 11:26:09 +0100

    videocall

---
+++ src/app/app.module.ts
@@ -0,0 +1,43 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MainMenuComponent } from './components/main-menu/main-menu.component';
+import { LoginComponent } from './components/login/login.component';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { CallService } from './services/call.service';
+import { CallinfoDialogComponent } from './dialog/callinfo-dialog/callinfo-dialog.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button'
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from "@angular/material/form-field"
+import { MatInputModule } from "@angular/material/input"
+import { FormsModule } from '@angular/forms';
+import { ClipboardModule } from '@angular/cdk/clipboard';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    MainMenuComponent,
+    CallinfoDialogComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ClipboardModule,
+    MatSnackBarModule
+  ],
+  providers: [
+    CallService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/components/main-menu/main-menu.component.ts
@@ -0,0 +1,61 @@
+import { Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { Observable, of } from 'rxjs';
+import { filter, switchMap } from 'rxjs/operators';
+import { CallService } from 'src/app/services/call.service';
+import { CallinfoDialogComponent, DialogData } from 'src/app/dialog/callinfo-dialog/callinfo-dialog.component';
+
+@Component({
+  selector: 'app-main-menu',
+  templateUrl: './main-menu.component.html',
+  styleUrls: ['./main-menu.component.css']
+})
+export class MainMenuComponent implements OnInit, OnDestroy {
+  public isCallStarted$: Observable<boolean>;
+  private peerId: string;
+
+  @ViewChild('localVideo')
+  localVideo!: ElementRef<HTMLVideoElement>;
+  @ViewChild('remoteVideo')
+  remoteVideo!: ElementRef<HTMLVideoElement>;
+
+  constructor(public dialog: MatDialog, private callService: CallService) { 
+    this.isCallStarted$ = this.callService.isCallStarted$;
+    this.peerId = this.callService.initPeer();
+  }
+
+  ngOnDestroy(): void {
+    this.callService.destroyPeer();
+  }
+
+  ngOnInit(): void {
+    this.callService.localStream$
+      .pipe(filter(res => !!res))
+      .subscribe((stream: MediaProvider | null) => this.localVideo.nativeElement.srcObject = stream)
+    this.callService.remoteStream$
+      .pipe(filter(res => !!res))
+      .subscribe((stream: MediaProvider | null) => this.remoteVideo.nativeElement.srcObject = stream)
+  }
+
+  public showModal(joinCall: boolean): void {
+    if(!this.peerId){  alert("Error en la comunicación"); return;}
+    let dialogData: DialogData = joinCall ? ({ peerId: undefined, joinCall: true }) : ({ peerId: this.peerId, joinCall: false });
+    const dialogRef = this.dialog.open(CallinfoDialogComponent, {
+      width: '250px',
+      data: dialogData
+    });
+
+    dialogRef.afterClosed()
+      .pipe(
+        switchMap(peerId => 
+          joinCall ? of(this.callService.establishMediaCall(peerId)) : of(this.callService.enableCallAnswer())
+        ),
+      )
+      .subscribe(_  => { });
+  }
+
+  public endCall() {
+    this.callService.closeMediaCall();
+  }
+
+}
